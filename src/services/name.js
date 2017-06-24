81c1fd5afa83f5c7d8f8d5599d7aca5d0e394b61
Miguel Pignatelli <emepyc@gmail.com>
2017-06-24 20:08:23 +0100

    Update name.js

--- src/services/name.js
+++ src/services/name.js
@@ -1,7 +1,7 @@
 /*
  * To change the name in the current version of the UI:
  * 1. Change the name below, commit and push (or just edit it from github)
- * 2. Go to https://app.netlify.com/sites/ranger-vincent-63775/deploys?filter=master and trigger a deploy
+ * 2. Netlify will automatically deploy the new name.
  * 3. Note that this won't change the domain name :-)
  */
 export default 'Library';
