90dabae5344fbe3adf7e66ff2aac8aa8357c5ab6
Miguel Pignatelli <emepyc@gmail.com>
2018-01-24 22:00:10 +0000

    using the production api

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,3 @@
-const apiBaseUrl = 'https://vy36p7a9ld.execute-api.eu-west-1.amazonaws.com/dev/';
+// const apiBaseUrl = 'https://vy36p7a9ld.execute-api.eu-west-1.amazonaws.com/dev/';
+const apiBaseUrl = 'http://link.opentargets.io';
 export default apiBaseUrl;
