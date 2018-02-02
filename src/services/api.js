d8626c418e96eda7604239978276be9dfeed41a5
Miguel Pignatelli <emepyc@gmail.com>
2018-02-02 12:55:47 +0100

    including trailing slash in url

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 // const apiBaseUrl = 'https://vy36p7a9ld.execute-api.eu-west-1.amazonaws.com/dev/';
-const apiBaseUrl = 'https://link.opentargets.io';
+const apiBaseUrl = 'https://link.opentargets.io/';
 export default apiBaseUrl;
